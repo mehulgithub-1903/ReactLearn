646763b25f9f8da8cf9b7f8bdd84f30e715b2510
Mehul Porwal <mehulporwal9999@gmail.com>
2021-05-07 15:08:08 +0530

    Props Part 1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import "./index.css";
-import App from './App.jsx'
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+import Card from './Cards';
+
+ReactDOM.render(<> 
+<Card imgsrc="https://fsa.zobj.net/crop.php?r=DUIVuL0OaIqj1Hu7N6nyUR_dGmY6fd-vRHFTJds_vabgSDeKyVuU95KNRIia9_yr8AWlIBJpgliOscW1jf-KomvCHRIyvqKDA5v__BxuLLHbcPWhPkBEqHP4yX6oFg2TB2KtziT9s-ieDYJa"
+    title="A Netflix original"
+    sname="Dark"
+    link="https://www.netflix.com/in/title/80100172"
+/>
+<Card
+     imgsrc="https://fsa.zobj.net/crop.php?r=DUIVuL0OaIqj1Hu7N6nyUR_dGmY6fd-vRHFTJds_vabgSDeKyVuU95KNRIia9_yr8AWlIBJpgliOscW1jf-KomvCHRIyvqKDA5v__BxuLLHbcPWhPkBEqHP4yX6oFg2TB2KtziT9s-ieDYJa"
+    title="A Netflix original"
+    sname="D2"
+    link="https://www.netflix.com/in/title/80100172"
+/>
+<Card imgsrc="https://fsa.zobj.net/crop.php?r=DUIVuL0OaIqj1Hu7N6nyUR_dGmY6fd-vRHFTJds_vabgSDeKyVuU95KNRIia9_yr8AWlIBJpgliOscW1jf-KomvCHRIyvqKDA5v__BxuLLHbcPWhPkBEqHP4yX6oFg2TB2KtziT9s-ieDYJa"
+    title="A Netflix original"
+    sname="Expo"
+    link="https://www.netflix.com/in/title/80100173"/>
+<Card 
+    imgsrc="https://fsa.zobj.net/crop.php?r=DUIVuL0OaIqj1Hu7N6nyUR_dGmY6fd-vRHFTJds_vabgSDeKyVuU95KNRIia9_yr8AWlIBJpgliOscW1jf-KomvCHRIyvqKDA5v__BxuLLHbcPWhPkBEqHP4yX6oFg2TB2KtziT9s-ieDYJa"
+    title="A Netflix original"
+    sname="Stranger Things"
+    link="https://www.netflix.com/in/title/80100171"
+
+/>
+
+</>,document.getElementById('root'));
