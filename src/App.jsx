2d056efb6408af31e025a8fa4a29a949d2fcae10
Mehul Porwal <mehulporwal9999@gmail.com>
2021-06-01 20:47:13 +0530

    login form basic

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,35 @@ import React ,{useState}from 'react'
 
 const App = () => {
 
-    
-    const [greet, setgreet] = useState("Hello");
 
-    const [fname, setfname] = useState()
-
-    const ChangeInput=(event)=>{
-        console.log(event.target.value);
-        setgreet(event.target.value); 
+    const [Name, setName] = useState()
+    const [pass, setPass] = useState()
+    const [fName, setfName] = useState()
+    const runForm=(event)=>{
+        event.preventDefault();
+        setfName(Name)
+        alert(pass)
+    }
+    const ChangeName=(event)=>{
+        setName(event.target.value); 
     }
 
-    // here event is onchange target is input and value is the value of the input
-    const ChangeValue=(event)=>{
-        setfname(greet)
-       
+    const ChangePass=(event)=>{
+        setPass(event.target.value)
+
     }
     
+   
     return (
         <div>
-            <h1> Hello {fname}</h1>
-            <input type="text" placeholder="Enter Your Name" onChange={ChangeInput} value={greet}/> 
-            <button onClick={ChangeValue}>Submit</button>  
+            <form onSubmit={runForm}>
+            <h1> Hello {fName}</h1>
+            <input type="text" placeholder="Enter Your Name" onChange={ChangeName} value={Name} /> 
+            <input type="text" placeholder="Enter Your Password" onChange={ChangePass} value={pass} /> 
+            <button type="submit">Submit</button>
+            </form>  
         </div>
     )
 }
 
-export default App
+export default App;
