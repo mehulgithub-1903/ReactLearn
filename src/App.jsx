6cfc774f69f07e541c2f5e37c35137392ed4b67f
Mehul Porwal <mehulporwal9999@gmail.com>
2021-06-05 22:41:49 +0530

    Todo List using material UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React, {useState} from 'react'
-import 'react-clock/dist/Clock.css';    
-import MyApp from './components/Clock'
-import Countdown from 'react-countdown';
+import TodoList from './components/TodoList'
 const App = () => {
 
     return (
         <div>
-        <Countdown date={Date.now() + 100000} />
+        <TodoList></TodoList>
         </div>
 
     )
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React ,{useState}from 'react'
+import Button from '@material-ui/core/Button';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import TodoListRender from './TodoListRender'
 
-const TodoList = (props) => {
+const TodoList = () => {
+
+
+    const [task, setTask] = useState()
+    const [Items, setItems] = useState([])
+
+    const UpdateTask=(event)=>{
+        setTask(event.target.value)
+
+    }
+
+ 
+    const TaskAdd=()=>{
+        setItems((oldItems)=>{
+            return [...oldItems,task]
+        })
+        setTask("")
+    }
+
+    // const DeleteItems=(id)=>{
+    //     setItems((oldItems)=>{  
+    //         return oldItems.filter((e,index)=>{
+    //            return index!==id                
+    //         })
+    //     })
+    // }
+    
     return (
-                   <>
-                   <li><button onClick={()=>
-                    {props.onSelect(props.id)}}>❎</button> {props.text}</li>
-                   </>
+        
+        <div>
+            <h1> TodoList</h1>
+            <br />
+            <input type="text" name="task" placeholder="Add Task" onChange={UpdateTask} value={task} />
+            <Button onClick={TaskAdd} color="secondary"> <AddCircleIcon></AddCircleIcon></Button>
+            {Items.map((e,index)=>{
+               return <TodoListRender
+                id={index} 
+               key={index} 
+               text={e}
+            //    onSelect={DeleteItems}    
+               />
+           })}
+        </div>
     )
 }
 
