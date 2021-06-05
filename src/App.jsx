84ac675d3be2c4dcbd5863dc0f9bfbe129d0abc0
Mehul Porwal <mehulporwal9999@gmail.com>
2021-06-05 09:47:58 +0530

    Countdown added using npm

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,12 @@
 import React, {useState} from 'react'
-import AddIcon from '@material-ui/icons/Add';
-import RemoveIcon from '@material-ui/icons/Remove';
-import Button from '@material-ui/core/Button';
-import Card from './components/Card';
-import Tooltip from '@material-ui/core/Tooltip';
+import 'react-clock/dist/Clock.css';    
+import MyApp from './components/Clock'
+import Countdown from 'react-countdown';
 const App = () => {
-
-
-    const [num, setNum] = useState(0);
-
-    const IncNum=()=>{
-        if(num!=10){
-            setNum(num+1)
-        }
-        else{
-            alert("sorry value cannot be further increased")
-        }
-    }
-
-
-    const DecNum=()=>{
-        if(num!=0){
-            setNum(num-1)
-        }else{
-            alert("sorry value cannot be further decreased")
-        }
-    }
-
 
     return (
         <div>
-        <p>{num}</p>
-        <Tooltip title="Add"> 
-            <Button className="inc" onClick={IncNum}> <AddIcon/> </Button>
-            </Tooltip>
-            <Tooltip title="Delete"> 
-            <Button className="dec" onClick={DecNum}> <RemoveIcon/></Button> 
-            </Tooltip>  
-                  
-        <Card></Card>
+        <Countdown date={Date.now() + 100000} />
         </div>
 
     )
--- src/index.js
+++ src/index.js
@@ -4,9 +4,4 @@ import App from './App'
 
 ReactDOM.render(<App/>,document.getElementById('root'));
 
-const arr1=["mehul","porwal","hello"]
-
-const arr2=["richa",...arr1]
-
-console.log(arr1)
-console.log(arr2)
+   
