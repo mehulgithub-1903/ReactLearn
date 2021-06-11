9273fdf5a47c05809769b60d7c0e45048069c90b
Mehul Porwal <mehulporwal9999@gmail.com>
2021-06-11 14:17:59 +0530

    This is a dummy hooks project

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,18 @@ import React,{useState,useEffect} from 'react'
 const App = () => {
 
 
-    const [name, setName] = useState(0);
+   
     const [num, setNum] = useState(0)
-     useEffect(()=>{
-         alert("Hello")
-        })
-
-        /*use effect gets triggered after render function in index
-            useEffect(()=>{
-                alert("Hello")
-                },[])
-        --> sirf pheli baar render hone pe useEffect Chalega 
-            useEffect(()=>{
-                alert("Hello")
-                },[num])
-        --> num ki value change hone pe hi chalega 
-
-        */
-    
+   
+    useEffect(()=>{
+        document.title=`You clicked me ${num} times`
+    })
     return (
         <div>
         <button onClick={()=>{
-            setName(name+1)
-        }}>Click me {name}</button>
-         <button onClick={()=>{
             setNum(num+1)
         }}>Click me {num}</button>
+        
         </div>
     )
 }
